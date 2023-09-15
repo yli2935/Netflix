866668de888d6fc0f51a156dad401d1fdbae9d31
Li Yaguang <yli2935@uwo.ca>
2023-09-15 01:04:17 -0400

    ClientOnly

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,33 +7,31 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
-import './globals.css'
+import "./globals.css";
 
-import Navbar from '@/app/components/navbar/Navbar';
-import { Nunito } from 'next/font/google'
+import Navbar from "@/app/components/navbar/Navbar";
+import { Nunito } from "next/font/google";
+import ClientOnly from "./components/ClientOnly";
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
-}
-const font = Nunito({ 
-  subsets: ['latin'], 
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+const font = Nunito({
+  subsets: ["latin"],
 });
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
-
   return (
     <html lang="en">
-      
       <body className={font.className}>
-        <Navbar />
-        <div className="pb-20 pt-28">
-          {children}
-        </div>
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%Alo
  */
 
-
+'use client';
 export default function Home() {
   return (
     <div className='text-rose-500 text-2xl'>hello</div>
